bb572483a9e62dfe57375fcff89859391c49b934
kishu <souravpoddar6677@gmail.com>
2025-04-11 21:15:37 +0530

    server is initialized

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <div className="h-screen ">
+        <div className="border h-[70px]"></div>
 
-export default App
+        <div className="ml-[100px] mt-[150px] flex flex-col display relative ">
+          <h1 className="text-9xl font-sans font-stretch-expanded main-title">
+            ScoreBoard
+          </h1>
+          <p className="text-4xl ml-2 font-mono main-promoline ">
+            Get Score of your Favourite Team
+          </p>
+          <div className="h-[100px] w-[100px] border-2 shadow-2xl shadow-gray-400 rounded-full main-ballAni  absolute"></div>
+        </div>
+      </div>
+    </>
+  );
+}
